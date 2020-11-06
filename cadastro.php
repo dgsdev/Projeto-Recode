7554292394362da974e37c59d374de1d0246136a
dgsdev <dgspramos@gmail.com>
2020-11-06 03:22:54 -0300

    Página cadastro e css

--- cadastro.php
+++ cadastro.php
@@ -10,10 +10,11 @@
      <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@500&display=swap" rel="stylesheet">
      <link href="https://fonts.googleapis.com/css2?family=Balsamiq+Sans:ital,wght@1,700&family=Playfair+Display:wght@500&display=swap" rel="stylesheet">
      <link rel="stylesheet" href="./css/style.css">
+     <link rel="stylesheet" href="./css/cadastro.css">
     </head>
 <body>
    
-<div class="container-fluid px-0">
+<div class="container-fluid px-0 d-flex justify-content-center">
     <video autoplay muted loop id="myVideo">
         <source src="./img/VID-20201102-WA0003.mp4" type="video/mp4">
         Your browser does not support HTML5 video.
@@ -54,22 +55,63 @@
                         <!--Fim do dropdown-->
             </nav>
                         <!-- Fim da Nav-menu -->
-
-                    <!-- começo da parte do texto -->
-            <div id="content" class="col-lg-6 col-md-6 col-sm-6">
-            <h2>JUMP</h2>
-            <p style="font-size:1.1rem;">Frase que resuma o projeto,mas que deixe claro sua intenção,proposta e quem queremos impactar.</p>
-            </div>
-                    <!-- fim da parte do texto -->
             
+                    <!--inicio formulário-->
+        <div id="cadastro" class="col-lg-6 col-md-8 col-sm-6">
+            <form method="post" action="">
 
+                <legend>Cadastre-se</legend>
+                <p>
+                    <label for="nome_cad">Digite seu nome completo:</label>
+                    <input id="nome_cad" name="nome" required="required" type="text" />
+                </p>
+
+                <p>
+                    <label for="identificacao_cad">Como quer ser identificado:</label>
+                    <input id="identificacao_cad" name="identificacao" required="required" type="text" />
+                </p>
+
+                <p>
+                    <label for="email_cad">Digite seu e-mail:</label>
+                    <input id="email_cad" name="email" required="required" type="email" />
+                </p>
+
+
+                <div class="senha">
+                    <label for="cad_senha">Digite sua senha</label>
+                    <input id="cad_senha" name="senha_cad" required="required" type="password" />
+                    <label for="conf_senha">Confirme sua senha</label>
+                    <input id="conf_senha" name="conf_senha" required="required" type="password" />
+                </div>
+                <br>
+
+                <!--cont... menu opções-->
+
+                <div class="checkbox">
+                    <p>Em qual situação se enquadra?</p>
+                    <input type="checkbox" value="" name="opcao1" id="opc_surdez" />
+                    <label for="checkbox">Surdez</label>
+                    <input type="checkbox" value="" name="opcao2" id="opc_def" />
+                    <label for="checkbox">Def. auditiva</label>
+                    <input type="checkbox" value="" name="opcao3" id="opc_nenhum" />
+                    <label for="checkbox">Nenhum</label>
+                    <input type="checkbox" value="" name="opcao4" id="opc_outros" />
+                    <label for="checkbox">Outros</label>
+                </div>
+
+                <!--fim formulário-->
+                
+                <br>
+                    <button type="submit" class="btn nova-cor">Enviar</button>
+            </form>
+
+        </div>
 
                     <!-- inicio do rodape -->
             <footer id="rodape">    
             <p>Copyright &copy; SQUAD-8 - Recode PRO - 2020</p>
 
                     <!-- fim do rodape -->
-                    
         </footer>
 </div>
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
