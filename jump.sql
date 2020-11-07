730d612f3495d6de9d1067d3e6addc4996169c57
dgsdev <dgspramos@gmail.com>
2020-11-06 22:51:13 -0300

    Arquivo Banco de Dados Sql

--- jump.sql
+++ jump.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Tempo de geração: 06/11/2020 às 15:46
+-- Tempo de geração: 07/11/2020 às 01:48
 -- Versão do servidor: 8.0.21
 -- Versão do PHP: 7.4.9
 
@@ -20,6 +20,8 @@ SET time_zone = "+00:00";
 --
 -- Banco de dados: `jump`
 --
+CREATE DATABASE IF NOT EXISTS `jump` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci;
+USE `jump`;
 
 -- --------------------------------------------------------
 
@@ -54,7 +56,7 @@ CREATE TABLE IF NOT EXISTS `usuarios` (
   `apelido` varchar(50) DEFAULT NULL,
   `imagem` varchar(150) DEFAULT NULL,
   `email` varchar(80) NOT NULL,
-  `senha` varchar(32) NOT NULL,
+  `senha` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   `conf_senha` varchar(32) NOT NULL,
   `situacao` varchar(50) NOT NULL,
   `grau` varchar(50) DEFAULT NULL,
@@ -69,7 +71,7 @@ CREATE TABLE IF NOT EXISTS `usuarios` (
   `data` datetime DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`idusuarios`),
   UNIQUE KEY `email_UNIQUE` (`email`)
-) ENGINE=MyISAM AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=17 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Despejando dados para a tabela `usuarios`
@@ -77,11 +79,17 @@ CREATE TABLE IF NOT EXISTS `usuarios` (
 
 INSERT INTO `usuarios` (`idusuarios`, `nome`, `apelido`, `imagem`, `email`, `senha`, `conf_senha`, `situacao`, `grau`, `sexo`, `nascimento`, `endereco`, `bairro`, `cidade`, `estado`, `cep`, `pais`, `data`) VALUES
 (1, 'Paulo', 'Paulinho', NULL, 'paulo@ongbr.com', 'e10adc3949ba59abbe56e057f20f883e', '', 'Surdo', 'Leve', 'Masculino', '2016-09-13', 'Rua B, 258', 'Palmares', 'Santo André', 'SP', '05555-333', 'Brasil', NULL),
-(2, 'douglas', 'dgs', '', 'dgs@dgsdev.com', 'e10adc3949ba59abbe56e057f20f883e', '', 'Ouvinte', 'Leve', 'Masculino', '0000-00-00', 'Rua K', 'Vila Mariana', 'São Paulo', 'SP', '09582-289', 'Brasil', NULL),
 (3, 'mateus', 'mateus', '', 'mateus@dgsdev.com', 'e10adc3949ba59abbe56e057f20f883e', '', 'Surdo', 'Outro', 'Masculino', '0000-00-00', 'Rua K', 'Vila Mariana', 'São Paulo', 'SP', '09582-289', 'Brasil', '0000-00-00 00:00:00'),
 (4, 'juca', 'juca', '', 'juca@dgsdev.com', 'e10adc3949ba59abbe56e057f20f883e', '', 'Ouvinte', 'Leve', 'Masculino', '0000-00-00', 'Rua K', 'Vila Mariana', 'São Paulo', 'SP', '09582-289', 'Brasil', '2020-11-06 00:32:27'),
 (5, 'joão', 'jo', '', 'joao@dgsdev.com', 'e10adc3949ba59abbe56e057f20f883e', '', 'Ouvinte', 'Leve', 'Masculino', '0000-00-00', 'Rua K', 'Vila Mariana', 'São Paulo', 'SP', '09582-289', 'Brasil', '2020-11-06 00:36:47'),
-(6, 'paulo p', 'paulinho', 'https://images.pexels.com/photos/358532/pexels-photo-358532.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500', 'paulinho@dgsdev.com', 'e10adc3949ba59abbe56e057f20f883e', '', '', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2020-11-06 04:05:10');
+(13, 'fulano', 'fulano', 'https://images.pexels.com/photos/358532/pexels-photo-358532.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500', 'fulano@dgsdev.com', 'e10adc3949ba59abbe56e057f20f883e', '', '', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2020-11-06 20:45:14'),
+(7, 'David', 'davi', 'https://cdn.pixabay.com/photo/2020/09/06/20/21/cat-5550033_960_720.jpg', 'davi@dgsdev.com', 'e10adc3949ba59abbe56e057f20f883e', '', '', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2020-11-06 13:15:15'),
+(8, 'carlos', 'carlos', '', 'carlos@dgsdev.com', 'e10adc3949ba59abbe56e057f20f883e', '', 'Surdo', 'Outro', 'Masculino', '0000-00-00', 'Rua K', 'Vila Mariana', 'São Paulo', 'SP', '09405410', 'Brasil', '2020-11-06 17:41:07'),
+(9, 'july', 'july', 'https://images.pexels.com/photos/358532/pexels-photo-358532.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500', 'july@dgsdev.com', 'e10adc3949ba59abbe56e057f20f883e', '', 'Ouvinte', 'Moderada', 'Feminino', '0000-00-00', 'Rua K', 'Vila Mariana', 'Suzano', 'SP', '05620-440', 'Brasil', '2020-11-06 17:55:13'),
+(10, 'julia julia', 'ju', 'https://cdn.pixabay.com/photo/2020/09/06/20/21/cat-5550033_960_720.jpg', 'julia@dgsdev.com', 'e10adc3949ba59abbe56e057f20f883e', '', '', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2020-11-06 18:02:54'),
+(11, 'antonio', 'antonio', 'https://cdn.pixabay.com/photo/2020/09/06/20/21/cat-5550033_960_720.jpg', 'antonio@dgsdev.com', 'e10adc3949ba59abbe56e057f20f883e', '', 'Surdo', 'Outro', 'Masculino', '0000-00-00', 'Rua K', 'Vila Mariana', 'Ribeirão Pires', 'SP', '09406290', 'Brasil', '2020-11-06 18:44:05'),
+(14, 'dgdgs', 'gsgsd', 'https://ik.imagekit.io/feq0hccnlg/JULIANACOLANTUONO_LOGO-16_TFZdgeZbS.png', 'dg@dgsdev.com', 'e10adc3949ba59abbe56e057f20f883e', '', '', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2020-11-06 20:46:48'),
+(16, '', '', '', '', 'd41d8cd98f00b204e9800998ecf8427e', '', '', '', '', '0000-00-00', '', '', '', '', '', '', '2020-11-06 21:36:50');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
