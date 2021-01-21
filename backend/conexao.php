56573c226c00926758d6fddc3955bf21f6cd2d69
dgsdev <dgspramos@gmail.com>
2021-01-21 02:46:36 -0300

    dormir r

--- backend/conexao.php
+++ backend/conexao.php
@@ -2,7 +2,7 @@
 
 $servername = "sql102.epizy.com";
 $username = "epiz_26883339";
-$password = "epiz_26883339";
+$password = "@DGSjuh12";
 $database = "epiz_26883339_jump";
 
 $link = mysqli_connect($servername, $username, $password, $database);
